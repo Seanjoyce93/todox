91e22b2d8f5f077fda419b9d7700191c3a3d2e52
“Sean <seanjoyce93@icloud.com>
2024-02-13 23:00:25 +0000

    Update .gitignore and layout.tsx, add todo component and database types

---
+++ app/components/todo.tsx
@@ -0,0 +1,23 @@
+import { TrashIcon } from "@heroicons/react/16/solid";
+import React from "react";
+
+type TodoProps = {
+  text: string;
+  completed: boolean;
+};
+
+export default function todo(props: TodoProps) {
+  const { text, completed } = props;
+  return (
+    <div className="flex flex-row items-center shadow-md p-3 ">
+      <div className="flex flex-row gap-2 items-center flex-grow">
+        <input type="checkbox" checked={completed} className="w-5 h-5"></input>
+        <label>{text}</label>
+      </div>
+
+      <button>
+        <TrashIcon className="h-6 w-6 text-red-600" />
+      </button>
+    </div>
+  );
+}
