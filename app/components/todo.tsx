727cf7e8482cc110ca968dad992cbef852218483
“Sean <seanjoyce93@icloud.com>
2024-02-13 23:05:57 +0000

    Add styling to todo component and update page header

--- app/components/todo.tsx
+++ app/components/todo.tsx
@@ -9,7 +9,7 @@ type TodoProps = {
 export default function todo(props: TodoProps) {
   const { text, completed } = props;
   return (
-    <div className="flex flex-row items-center shadow-md p-3 ">
+    <div className="flex flex-row items-center rounded shadow-md p-3 ">
       <div className="flex flex-row gap-2 items-center flex-grow">
         <input type="checkbox" checked={completed} className="w-5 h-5"></input>
         <label>{text}</label>
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Todo from "./components/todo";
 export default function Home() {
   return (
     <main className=" p-1 mt-2 box-border ">
+      <h1 className="font-bold text-3xl mb-2">Todox</h1>
+      <div className="border-b border-gray-200 mb-4 mt-2"></div>
       <section className="flex flex-col  item-center gap-2">
         <Todo text="hello world" completed />
         <Todo text="hello world" completed />
